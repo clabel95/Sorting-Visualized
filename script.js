955eb1f3186bd279cda40ba4b4a1808115a1c35b
Clay Abel <clayton.abel.95@gmail.com>
2022-04-13 18:00:43 -0500

    finaly figured out how to get the window to update while an event was still active so the graph updates as it sorts. Also got the graph bars to change color to better show how the sorting is being done.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let delay = 0;
 
 
 var choice = document.getElementById("algo");
-//var choice_v = choice.options[choice.selectedIndex].value;
 
-var barColors = "red";
+
+var barColors = [];
 var yValues = [9, 10, 15, 23, 4, 13, 66]
 
 const Visualization = new Chart(ctx, {
@@ -38,22 +38,24 @@ const Visualization = new Chart(ctx, {
     }
 });
 
-
+console.log(Visualization.data.datasets[0].backgroundColor)
 
 function removeData(chart) {
-    while (chart.data.labels.length !== 0) {
+    while (chart.data.labels.length !== 0 && chart.data.datasets[0].backgroundColor !== 0) {
         chart.data.labels.pop();
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.pop();
-        });
+        // chart.data.datasets.forEach((dataset) => {
+        //     dataset.data.pop();
+        // });
+        chart.data.datasets[0].backgroundColor.pop();
         chart.update("none");
     }
-
 }
-function addData(chart, label, data) {
+function addData(chart, label, color) {
+    //console.log(chart.data.datasets[0].backgroundColor)
     let l = label.length
     for (let i = 0; i < l; i++) {
         chart.data.labels.push(label[i]);
+        chart.data.datasets[0].backgroundColor.push(color[i]);
     }
     delay = delay - 1;
     chart.update("none");
@@ -63,18 +65,25 @@ async function bubbleSort(arr) {
     let l = arr.length;
     for (let i = 0; i < l; i++) {
         for (let j = 0; j < l - i - 1; j++) {
+            barColors[j] = "red";
             if (arr[j + 1] < arr[j]) {
                 [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
             }
-            UpdateNumbers(arr);
+            barColors[j] = "red";
+            barColors[j + 1] = "green";
+            UpdateNumbers(arr, barColors);
+            //console.log(barColors)
+
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
-                }, 300)
+                }, 150)
             );
 
         }
     };
+    barColors = (Array.from({ length: l }, () => "green"))
+    UpdateNumbers(arr, barColors);
     return arr;
 };
 
@@ -82,17 +91,27 @@ async function insertionSort(arr) {
     let l = arr.length;
     for (let i = 1; i < l; i++) {
         for (let j = i - 1; j > -1; j--) {
-            if (arr[j + 1] < arr[j]) {
-                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
-            }
-            UpdateNumbers(arr);
+            
+            barColors[j+1] = "blue"
+            barColors[j] = "orange"
+            barColors[i] = "yellow"
+            UpdateNumbers(arr, barColors);
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
-                }, 300)
+                }, 200)
             );
+            barColors[j+1] = "orange"
+            if (arr[j + 1] < arr[j]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+            }
         }
+
     };
+
+    barColors = (Array.from({ length: l }, () => "green"))
+
+    UpdateNumbers(arr, barColors);
     return arr;
 };
 
@@ -103,6 +122,17 @@ async function selectionSort(arr) {
     for (let i = 0; i < l; i++) {
         min = i;
         for (let j = i + 1; j < l; j++) {
+            barColors[j] = "yellow";
+            barColors[min] = "blue";
+            UpdateNumbers(arr, barColors);
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, 300)
+            );
+            barColors[j] = "red";
+            barColors[min] = "red";
+            
             if (arr[j] < arr[min]) {
                 min = j;
             }
@@ -110,13 +140,18 @@ async function selectionSort(arr) {
         if (min !== i) {
             [arr[i], arr[min]] = [arr[min], arr[i]];
         }
-        UpdateNumbers(arr);
+        barColors[i] = "green"
+        UpdateNumbers(arr, barColors);
+        //barColors[min] = "red"
         await new Promise((resolve) =>
             setTimeout(() => {
                 resolve();
             }, 300)
         );
     }
+    barColors = (Array.from({ length: l }, () => "green"))
+    UpdateNumbers(arr, barColors);
+
     return arr;
 }
 
@@ -133,19 +168,19 @@ function SortChoice() {
 
 function NewNumbers() {
     var quantityValue = document.querySelector("#sortSize");
+    barColors = (Array.from({ length: quantityValue.value }, () => "red"))
     yValues = (Array.from({ length: quantityValue.value }, () => (Math.floor(Math.random() * (1000 - 1) + 1))))
     removeData(Visualization);
-    addData(Visualization, yValues)
-    return yValues;
+    addData(Visualization, yValues, barColors)
+    return yValues, barColors;
 }
 
 //using this function any sort function can update the chart.
 //this way we can update the chart every time a datapoint moves.
-function UpdateNumbers(data) {
+function UpdateNumbers(data, colors) {
     removeData(Visualization);
-    addData(Visualization, data);
+    addData(Visualization, data, colors);
 }
-
 
 randomize.addEventListener("click", NewNumbers);
 sort.addEventListener("click", SortChoice);
