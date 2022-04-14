b02251843b6d9a72efcb8e6f1bcde1f66e30a61e
Clay Abel <clayton.abel.95@gmail.com>
2022-04-14 12:44:43 -0500

    added the ability for the user to choose the speed of the sort so they can speed up or slow it down to be able to view it better.

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 var ctx = document.getElementById("Sort_Graph").getContext("2d");
 var randomize = document.querySelector("#randomize");
+var fresh_numbers = document.querySelector("#Fresh_Numbers")
+var sort_options = document.querySelector("#Sort_Options")
 var sort = document.querySelector("#sort_btn");
-let delay = 0;
 
 
-var choice = document.getElementById("algo");
+var choice = document.querySelector("#algo");
+var speed = document.querySelector("#speed");
 
 
 var barColors = [];
 var yValues = [9, 10, 15, 23, 4, 13, 66]
+
+
+
 
 const Visualization = new Chart(ctx, {
     type: "bar",
@@ -38,70 +43,39 @@ const Visualization = new Chart(ctx, {
     }
 });
 
-console.log(Visualization.data.datasets[0].backgroundColor)
-
 function removeData(chart) {
     while (chart.data.labels.length !== 0 && chart.data.datasets[0].backgroundColor !== 0) {
         chart.data.labels.pop();
-        // chart.data.datasets.forEach((dataset) => {
-        //     dataset.data.pop();
-        // });
         chart.data.datasets[0].backgroundColor.pop();
         chart.update("none");
     }
-}
+};
+
 function addData(chart, label, color) {
-    //console.log(chart.data.datasets[0].backgroundColor)
     let l = label.length
     for (let i = 0; i < l; i++) {
         chart.data.labels.push(label[i]);
         chart.data.datasets[0].backgroundColor.push(color[i]);
     }
-    delay = delay - 1;
     chart.update("none");
-}
-
-async function bubbleSort(arr) {
-    let l = arr.length;
-    for (let i = 0; i < l; i++) {
-        for (let j = 0; j < l - i - 1; j++) {
-            barColors[j] = "red";
-            if (arr[j + 1] < arr[j]) {
-                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
-            }
-            barColors[j] = "red";
-            barColors[j + 1] = "green";
-            UpdateNumbers(arr, barColors);
-            //console.log(barColors)
-
-            await new Promise((resolve) =>
-                setTimeout(() => {
-                    resolve();
-                }, 200)
-            );
-
-        }
-    };
-    barColors = (Array.from({ length: l }, () => "green"))
-    UpdateNumbers(arr, barColors);
-    return arr;
 };
 
 async function insertionSort(arr) {
+    console.log(speed.options[choice.selectedIndex])
     let l = arr.length;
     for (let i = 1; i < l; i++) {
         for (let j = i - 1; j > -1; j--) {
-            
-            barColors[j+1] = "blue"
+
+            barColors[j + 1] = "blue"
             barColors[j] = "orange"
             barColors[i] = "yellow"
             UpdateNumbers(arr, barColors);
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
-                }, 200)
+                }, speed.options[speed.selectedIndex].value)
             );
-            barColors[j+1] = "orange"
+            barColors[j + 1] = "orange"
             if (arr[j + 1] < arr[j]) {
                 [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
             }
@@ -112,9 +86,44 @@ async function insertionSort(arr) {
     barColors = (Array.from({ length: l }, () => "green"))
 
     UpdateNumbers(arr, barColors);
+    fresh_numbers.style.display = "inline-block"
     return arr;
 };
 
+async function bubbleSort(arr) {
+    let l = arr.length;
+    for (let i = 0; i < l; i++) {
+        for (let j = 0; j < l - i - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
+            }
+            barColors[j] = "red";
+            barColors[j + 1] = "blue";
+            UpdateNumbers(arr, barColors);
+
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, speed.options[speed.selectedIndex].value)
+            );
+            if (j == (l - i - 2)) {
+                barColors[j + 1] = "green"
+                UpdateNumbers(arr, barColors);
+
+                await new Promise((resolve) =>
+                    setTimeout(() => {
+                        resolve();
+                    }, speed.options[speed.selectedIndex].value)
+                );
+            }
+
+        }
+    };
+    barColors = (Array.from({ length: l }, () => "green"))
+    UpdateNumbers(arr, barColors);
+    fresh_numbers.style.display = "inline-block"
+    return arr;
+};
 
 async function selectionSort(arr) {
     let min;
@@ -128,11 +137,11 @@ async function selectionSort(arr) {
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
-                }, 200)
+                }, speed.options[speed.selectedIndex].value)
             );
             barColors[j] = "red";
             barColors[min] = "red";
-            
+
             if (arr[j] < arr[min]) {
                 min = j;
             }
@@ -146,16 +155,17 @@ async function selectionSort(arr) {
         await new Promise((resolve) =>
             setTimeout(() => {
                 resolve();
-            }, 300)
+            }, speed.options[speed.selectedIndex].value)
         );
     }
     barColors = (Array.from({ length: l }, () => "green"))
     UpdateNumbers(arr, barColors);
-
+    fresh_numbers.style.display = "inline-block"
     return arr;
-}
+};
 
 function SortChoice() {
+    sort_options.style.display = "none";
     switch (choice.options[choice.selectedIndex].value) {
         case "bubble": bubbleSort(yValues); break;
         case "selection": selectionSort(yValues); break;
@@ -163,24 +173,26 @@ function SortChoice() {
         case "quick": break;
         case "binary": break;
     }
-}
 
+};
 
 function NewNumbers() {
+    fresh_numbers.style.display = "none"
     var quantityValue = document.querySelector("#sortSize");
     barColors = (Array.from({ length: quantityValue.value }, () => "red"))
     yValues = (Array.from({ length: quantityValue.value }, () => (Math.floor(Math.random() * (1000 - 1) + 1))))
     removeData(Visualization);
     addData(Visualization, yValues, barColors)
+    sort_options.style.display = "inline-block";
     return yValues, barColors;
-}
+};
 
 //using this function any sort function can update the chart.
 //this way we can update the chart every time a datapoint moves.
 function UpdateNumbers(data, colors) {
     removeData(Visualization);
     addData(Visualization, data, colors);
-}
+};
 
 randomize.addEventListener("click", NewNumbers);
 sort.addEventListener("click", SortChoice);
