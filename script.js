4d164c8d485147457870640a874c76d7ab5e0f67
Clay Abel <clayton.abel.95@gmail.com>
2022-04-19 15:36:46 -0500

    added a new sorting algo called radixSort (probably my favorite so far) and updated the maximum values that will be created to be between 1 and 10 million to better show off how cool radixSort is.

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ const Visualization = new Chart(ctx, {
 function removeData(chart) {
     while (chart.data.labels.length !== 0 && chart.data.datasets[0].backgroundColor !== 0) {
         chart.data.labels.pop();
-        chart.data.datasets[0].backgroundColor.pop(); 
+        chart.data.datasets[0].backgroundColor.pop();
     }
 };
 
@@ -98,10 +98,10 @@ async function insertionSortND(arr) {
 
     };
     var end = performance.now();
-    if (end-start == 0){
-        algo_time.innerText= `Total sort time: < .1 milliseconds`;
-    }else{
-        algo_time.innerText= `Total sort time: ~${Math.round((end-start)*100)/100} milliseconds`;
+    if (end - start == 0) {
+        algo_time.innerText = `Total sort time: < .1 milliseconds`;
+    } else {
+        algo_time.innerText = `Total sort time: ~${Math.round((end - start) * 100) / 100} milliseconds`;
     }
     barColors = (Array.from({ length: l }, () => "green"))
     UpdateNumbers(arr, barColors);
@@ -158,10 +158,10 @@ async function bubbleSortND(arr) {
         }
     };
     var end = performance.now();
-    if (end-start == 0){
-        algo_time.innerText= `Total sort time: < .1 milliseconds`;
-    }else{
-        algo_time.innerText= `Total sort time: ~${Math.round((end-start)*100)/100} milliseconds`;
+    if (end - start == 0) {
+        algo_time.innerText = `Total sort time: < .1 milliseconds`;
+    } else {
+        algo_time.innerText = `Total sort time: ~${Math.round((end - start) * 100) / 100} milliseconds`;
     }
     barColors = (Array.from({ length: l }, () => "green"))
     UpdateNumbers(arr, barColors);
@@ -225,13 +225,13 @@ async function selectionSortND(arr) {
         }
     }
     var end = performance.now();
-    
-    if (end-start == 0){
-        algo_time.innerText= `Total sort time: < .1 milliseconds`;
-    }else{
-        algo_time.innerText= `Total sort time: ~${Math.round((end-start)*100)/100} milliseconds`;
+
+    if (end - start == 0) {
+        algo_time.innerText = `Total sort time: < .1 milliseconds`;
+    } else {
+        algo_time.innerText = `Total sort time: ~${Math.round((end - start) * 100) / 100} milliseconds`;
     }
-    
+
     barColors = (Array.from({ length: l }, () => "green"))
     UpdateNumbers(arr, barColors);
     fresh_numbers.style.display = "inline-block"
@@ -239,12 +239,109 @@ async function selectionSortND(arr) {
     return arr;
 };
 
+function getDigit(num, place) {
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10
+}
+
+function digitCount(num) {
+    if (num === 0) return 1
+    return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+function mostDigits(nums) {
+    let maxDigits = 0
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+    }
+    return maxDigits
+}
+
+async function radixSort(arrOfNums) {
+    let maxDigitCount = mostDigits(arrOfNums)
+    let l = arrOfNums.length;
+    for (let k = 0; k < maxDigitCount; k++) {
+        //if we want to do this with binary or even hex we would have to set the length to be a variable that we can change
+        let digitBuckets = Array.from({ length: 10 }, () => []) // [[], [], [],...]
+        let colorBuckets = Array.from({ length: 10 }, () => [])
+        for (let i = 0; i < l; i++) {
+            let digit = getDigit(arrOfNums[i], k)
+            digitBuckets[digit].push(arrOfNums[i])
+            //if we wanted to do different bases then we would have to add move values to this to account for hex.
+            switch(digit){
+                case 0: colorBuckets[digit].push("#00FFFF"); break;
+                case 1: colorBuckets[digit].push("#0000FF"); break;
+                case 2: colorBuckets[digit].push("#00008B"); break;
+                case 3: colorBuckets[digit].push("#ADD8E6"); break;
+                case 4: colorBuckets[digit].push("#800080"); break;
+                case 5: colorBuckets[digit].push("#FF00FF"); break;
+                case 6: colorBuckets[digit].push("#FFC0CB"); break;
+                case 7: colorBuckets[digit].push("#FFA500"); break;
+                case 8: colorBuckets[digit].push("#FFFF00"); break;
+                case 9: colorBuckets[digit].push("#800000"); break;
+            }
+        }
+        // New order after each loop
+        arrOfNums = [].concat(...digitBuckets)
+        barColors = [].concat(...colorBuckets)
+
+        UpdateNumbers(arrOfNums, barColors);
+        await new Promise((resolve) =>
+            setTimeout(() => {
+                resolve();
+            }, speed.options[speed.selectedIndex].value)
+        );
+
+    }
+    barColors = (Array.from({ length: l }, () => "green"))
+    UpdateNumbers(arrOfNums, barColors);
+    fresh_numbers.style.display = "inline-block"
+}
+
+
+async function radixSortND(arrOfNums) {
+    let maxDigitCount = mostDigits(arrOfNums)
+    let l = arrOfNums.length;
+
+    var start = performance.now();
+    for (let k = 0; k < maxDigitCount; k++) {
+        //if we want to do this with binary or even hex we would have to set the length to be a variable that we can change
+        let digitBuckets = Array.from({ length: 10 }, () => []) // [[], [], [],...]
+        for (let i = 0; i < l; i++) {
+            let digit = getDigit(arrOfNums[i], k)
+            digitBuckets[digit].push(arrOfNums[i])
+        }
+        // New order after each loop
+        arrOfNums = [].concat(...digitBuckets)
+    }
+
+    var end = performance.now();
+    if (end - start == 0) {
+        algo_time.innerText = `Total sort time: < .1 milliseconds`;
+    } else {
+        algo_time.innerText = `Total sort time: ~${Math.round((end - start) * 100) / 100} milliseconds`;
+    }
+
+    barColors = (Array.from({ length: l }, () => "green"))
+    UpdateNumbers(arrOfNums, barColors);
+    fresh_numbers.style.display = "inline-block"
+    algo_time.style.display = "flex";
+}
+
+
+
+
+
+
+
+
+
 function instantSortChoice() {
     sort_options.style.display = "none";
     switch (choice.options[choice.selectedIndex].value) {
         case "bubble": bubbleSortND(yValues); break;
         case "selection": selectionSortND(yValues); break;
         case "insertion": insertionSortND(yValues); break;
+        case "radixSort": radixSortND(yValues); break;
     }
 };
 
@@ -255,6 +352,7 @@ function SortChoice() {
         case "bubble": bubbleSort(yValues); break;
         case "selection": selectionSort(yValues); break;
         case "insertion": insertionSort(yValues); break;
+        case "radixSort": radixSort(yValues); break;
     }
 };
 
@@ -263,7 +361,7 @@ function NewNumbers() {
     algo_time.style.display = "none";
     var quantityValue = document.querySelector("#sortSize");
     barColors = (Array.from({ length: quantityValue.value }, () => "red"))
-    yValues = (Array.from({ length: quantityValue.value }, () => (Math.floor(Math.random() * (1000 - 1) + 1))))
+    yValues = (Array.from({ length: quantityValue.value }, () => (Math.floor(Math.random() * (10000000 - 1) + 1))))
     removeData(Visualization);
     addData(Visualization, yValues, barColors)
     sort_options.style.display = "inline-block";
